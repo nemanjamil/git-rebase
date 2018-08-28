f58de32781ca0cb605ecf904c3dc94ef14ea538c
nemanjamil <nemanjamil@gmail.com>
2018-08-28 23:08:00 +0200

    Git BranchInit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) =>
-    res.send('Hello World, HI')
+    res.send('Hello World. La')
 );
 
 app.listen(3000, () =>
