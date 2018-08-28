a10e29b7a131a522305482f3677adf3657032136
nemanjamil <nemanjamil@gmail.com>
2018-08-28 23:27:10 +0200

    2232

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) =>
-    res.send('Hello World, Master Change')
+    res.send('Hello World, Master Change ada asd')
 );
 
 app.listen(3000, () =>
